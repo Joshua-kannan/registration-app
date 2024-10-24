769b8831caa9a4d3933a1d61a96d34e5e38f6c05
joshua-kannan <joshuakannanwork@gmail.com>
2024-10-25 00:29:52 +0530

    Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule, Routes } from '@angular/router';  // Import RouterModule and Routes
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { RegistrationFormComponent } from './registration-form/registration-form.component';  // Import the registration form component
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
-// Define routes for the application
 const routes: Routes = [
   { path: '', redirectTo: '/register', pathMatch: 'full' },
   { path: 'register', component: RegistrationFormComponent }
@@ -31,7 +29,7 @@ const routes: Routes = [
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
-    RouterModule.forRoot(routes)  // Import RouterModule and configure it with routes
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
